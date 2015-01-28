4050ffd376217ee376e8ab53cc835bc226db4850
Walter Forkel <wko@forkel.eu>
2015-01-28 09:23:06 +0200

    Omer: Should work. Might be recurring problems with the video quality, but something that we've already solved and has come back, so should be easy.

--- hw4sol-vanhandel-dror/construct_video_from_pyramid.m
+++ hw4sol-vanhandel-dror/construct_video_from_pyramid.m
@@ -1,7 +1,9 @@
-function construct_video_from_pyramid(pyramid,filename_out, FrameRate)
+function construct_video_from_pyramid(pyramid,pyramid_frame_sizes,filename_out, FrameRate)
     % takes a cell arrray of size 5, where each cell is a video stream
     % of the respective level of the Laplacian pyramid, reconstructs
     % the image at every frame, and writes the reconstructed video to file.
+    % pyramid_frame_sizes is a cell array that defines the size of a frame 
+    % (x,y,colors) for each level in the pyramid.
     a = 0.6;
     d = 4;
 
@@ -11,17 +13,22 @@ function construct_video_from_pyramid(pyramid,filename_out, FrameRate)
     
     % preallocate memory
     L = cell(1,d+1); % Preallocate the pyramid
-    
-    
+        
     for frame_idx=1:size(pyramid{1},4) % for every frame in the video
         for level=1:d+1
-            L{level} = pyramid{level}(:,:,:,frame_idx);
+            % L{level} = pyramid{level}(:,:,:,frame_idx);
+            frame_size = prod(pyramid_frame_sizes{level});
+            output_frame = full(pyramid{level}(frame_size*(frame_idx-1)+1:frame_size*frame_idx));
+            L{level} = reshape(output_frame, ...
+                   pyramid_frame_sizes{level}(1), ...
+                   pyramid_frame_sizes{level}(2), ...
+                   pyramid_frame_sizes{level}(3));
         end;
 
         I_after = L_pyramid_decode(L,a);
         %TODO  (OT & WALTER) - mabye 'abs' insteat of 'subplus'
-        %writeVideo(writer, im2frame(im2uint8(subplus(I_after))));
-        writeVideo(writer, im2frame(im2uint8(abs(I_after))));
+        writeVideo(writer, im2frame(im2uint8(uint8(subplus(255*I_after)))));
+        %writeVideo(writer, im2frame(im2uint8(abs(I_after))));
     end;
     close(writer);    
 end
--- hw4sol-vanhandel-dror/temporal_filter.m
+++ hw4sol-vanhandel-dror/temporal_filter.m
@@ -1,38 +1,42 @@
-function [ filtered_matrix ] = temporal_filter (spatial_time_matrix, frequency_mask )
-    % Transpose the s.t.m to get the fft of every time vector
-    % since fft of a matrix returns the fft of every column of the matrix
-    
-%     spatial_freq_matrix = fft(spatial_time_matrix,[],2);
-%     repl_mask = repmat(frequency_mask, length(frequency_mask),1)
-%     filtered_matrix = ifft((spatial_freq_matrix .* repl_mask'),[],2);
-    
-    filtered_matrix = zeros(size(spatial_time_matrix),'double');
-    fprintf('Running until %d\n', size(spatial_time_matrix,1));
-    for i=1:size(spatial_time_matrix,1)
-        spatial_freq_matrix = fft(spatial_time_matrix(i,:));
-        filtered_matrix(i,:) = ifft(spatial_freq_matrix .* frequency_mask);
-        if (mod(i,100000) == 0) 
-            fprintf('%d\n ', i);
-        end;
-    end;
+% function [ filtered_matrix ] = temporal_filter (spatial_time_matrix, frequency_mask )
+%     % Transpose the s.t.m to get the fft of every time vector
+%     % since fft of a matrix returns the fft of every column of the matrix
+%     
+% %     spatial_freq_matrix = fft(spatial_time_matrix,[],2);
+% %     repl_mask = repmat(frequency_mask, length(frequency_mask),1)
+% %     filtered_matrix = ifft((spatial_freq_matrix .* repl_mask'),[],2);
+%     
+%     filtered_matrix = zeros(size(spatial_time_matrix),'double');
+%     fprintf('Running until %d\n', size(spatial_time_matrix,1));
+%     for i=1:size(spatial_time_matrix,1)
+%         spatial_freq_matrix = fft(spatial_time_matrix(i,:));
+%         filtered_matrix(i,:) = ifft(spatial_freq_matrix .* frequency_mask);
+%         if (mod(i,100000) == 0) 
+%             fprintf('%d\n ', i);
+%         end;
+%     end;
+% end
+
+function [ filtered_matrix ] = temporal_filter (spatial_time_matrix, frequency_mask)
+%     Transpose the s.t.m to get the fft of every time vector
+%     since fft of a matrix returns the fft of every column of the matrix
+
+    pixels = size(spatial_time_matrix,1);
+    NUM_PARALLEL_COLS = floor(pixels/ ceil(pixels/1e5));
+    size_stm = size(spatial_time_matrix);
+    filtered_matrix = sparse(size_stm(1),size_stm(2));%,'double');
+    %mask = single(ones(NUM_PARALLEL_COLS,1) *frequency_mask);
+
+    for i = 1:floor(pixels/NUM_PARALLEL_COLS)
+        a = (i-1) * NUM_PARALLEL_COLS + 1;
+        b = i * NUM_PARALLEL_COLS;
+        fprintf('%g-%g of %d\n',a,b, pixels);
+        spatial_freq_matrix = fft(spatial_time_matrix(a:b,:),[],2);
+        %tmp = sparse(ifft(spatial_freq_matrix .* mask,[], 2));
+        tmp = sparse(abs(ifft(bsxfun(@times,spatial_freq_matrix, frequency_mask),[], 2)));
+        tmp = (tmp .* (abs(tmp)>=1e-4));
+        filtered_matrix(a:b,:) = tmp; 
+        % (keep the matrix sparse by zeroing out everything thats less 1e-4)
+    end
+
 end
-
-%function [ filtered_matrix ] = temporal_filter (spatial_time_matrix, frequency_mask)
-    % Transpose the s.t.m to get the fft of every time vector
-    % since fft of a matrix returns the fft of every column of the matrix
-
-%     pixels = size(spatial_time_matrix,1);
-%     NUM_PARALLEL_COLS = floor(pixels/ ceil(pixels/5e4));
-%     filtered_matrix = zeros(size(spatial_time_matrix),'double');%'uint8');
-%     mask = ones(NUM_PARALLEL_COLS,1) *frequency_mask;
-% 
-% 
-%     for i = 1:floor(pixels/NUM_PARALLEL_COLS)
-%         a = (i-1) * NUM_PARALLEL_COLS + 1;
-%         b = i * NUM_PARALLEL_COLS;
-%         fprintf('%g-%g of %d\n',a,b, pixels);
-%         spatial_freq_matrix = fft(spatial_time_matrix(a:b,:),[],2);
-%         filtered_matrix(a:b,:) = abs(ifft(spatial_freq_matrix .* mask,[], 2));
-%     end
-
-%end
